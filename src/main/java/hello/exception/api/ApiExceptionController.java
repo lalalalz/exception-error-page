f53bd7b68c20834507d4e371e562875c62334a37
lalalalz <lalalalz@naver.com>
2022-05-10 12:03:29 +0900

    #ADD
    1. @ControllerAdvice 사용하여 예외 처리 컨트롤러 분리하기

--- src/main/java/hello/exception/api/ApiExceptionController.java
+++ src/main/java/hello/exception/api/ApiExceptionController.java
@@ -1,15 +1,12 @@
 package hello.exception.api;
 
-import hello.exception.exception.BadRequestException;
 import hello.exception.exception.UserException;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
@@ -31,65 +28,6 @@ public class ApiExceptionController {
         return new MemberDto(id, "hello" + id);
     }
 
-    @GetMapping("/api/response-status-ex1")
-    public String responseStatusEx1() {
-        throw new BadRequestException();
-    }
-
-    @GetMapping("/api/response-status-ex2")
-    public String responseStatusEx2() {
-        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "error.bad", new IllegalArgumentException());
-    }
-
-    @GetMapping("/api/default-handler-ex")
-    public String defaultException(@RequestParam Integer data) {
-        return "ok";
-    }
-
-//    @ExceptionHandler({IllegalArgumentException.class, BadRequestException.class})
-//    public ErrorResult multiEx(Exception e) {
-//        log.info("exception e", e);
-//        return new ErrorResult("BAD", "Multi Ex : " + e.getClass());
-//    }
-//
-//    @ExceptionHandler
-//    public ErrorResult userExHandle(UserException e) {
-//        log.info("exception e", e);
-//        return new ErrorResult("BAD", e.getMessage());
-//    }
-
-//    @ExceptionHandler(UserException.class)
-//    public ModelAndView userExViewHandle(UserException e) {
-//        return new ModelAndView("error/500");
-//    }
-
-//    @ExceptionHandler(UserException.class)
-//    public ResponseEntity<ErrorResult> userExHandle(UserException e) {
-//        ErrorResult errorResult = new ErrorResult("USER-EX", e.getMessage());
-//        return new ResponseEntity<>(errorResult, HttpStatus.BAD_REQUEST);
-//    }
-
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(UserException.class)
-    public String userExHandle(UserException e) {
-        return "ok";
-    }
-
-
-//    @ExceptionHandler(IllegalArgumentException.class)
-//    public ErrorResult illegalExHandler(IllegalArgumentException e) {
-//        log.error("[exceptionHandle] ex", e);
-//        return new ErrorResult("BAD", e.getMessage());
-//    }
-
-    @Data
-    @AllArgsConstructor
-
-    static class ErrorResult {
-        private String code;
-        private String message;
-    }
-
     @Data
     @AllArgsConstructor
     static class MemberDto {
@@ -97,4 +35,3 @@ public class ApiExceptionController {
         private String name;
     }
 }
-
